6ad9c43b59eda0e8013ef21bd920319e07f2f54f
Rangel Ivanov <radito3@mail.bg>
2018-04-04 16:43:11 +0300

    Test reading & writing files

--- request.py
+++ request.py
@@ -2,6 +2,28 @@ import requests
 
 r = requests.get("https://raw.githubusercontent.com/radito3/digitalLibrary/master/test.txt")
 
-print r.status_code
-print r.content
+# print(r.status_code) -> 200
+content = r.content.splitlines()
+for line in content:
+    print(line.decode('UTF-8'))
 
+
+with open('test.txt', 'rb') as f:
+    read_data = f.read()
+
+# print(read_data)
+
+with open('new.txt', 'x') as f:
+    # print(type(read_data))
+    f.write(repr(read_data.decode('UTF-8')))
+
+with open('new.txt') as f:
+    read_data = f.read()
+
+print(read_data)
+# for x in range(1, 11):
+#     print('{0:2d} {1:3d} {2:4d}'.format(x, x*x, x*x*x)) -> second digit is for mininum column width, letter is for type
+
+# table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+# for name, phone in table.items():
+#     print('{0:10} ==> {1:10d}'.format(name, phone))
